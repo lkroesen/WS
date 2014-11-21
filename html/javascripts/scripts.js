b247432d729ac1cc14ecfa17c97ef49f3b2964da
Lars Stegman <l.s.stegman@student.tudelft.nl>
2014-11-21 15:09:54 +0100

    Javascript is toegevoegd.
    Er kan een to do worden toegevoegd.
    Een to do kan worden verwijderd. Er moet dubbel geklikt worden om te
    verwijderen.

---
+++ html/javascripts/scripts.js
@@ -0,0 +1,64 @@
+
+
+//This method adds a to do and takes one vairable that is the message.
+var addToDo = function(message) {
+	if(message !== "") {
+		var todo = "<li class='todo'><input type='checkbox' />";
+		todo += message;
+		todo += "<button>EDIT</button><br /><button class='delete'>| |</button></li>";
+		$("#todos ul").append(todo);
+		$("#toDoMessage").val("");
+	}
+	
+}
+
+$(document).ready(function() {
+	//This method checks if the enterkey is pressed to add a to do to the list.
+	
+	$('#toDoMessage').on('keypress', function(e) {
+		//Check if enter was pressed. If so, continue.
+		if(e.which === 13) {
+			
+			var foo = $("#toDoMessage").val();
+			
+			addToDo(foo);	
+			
+			console.log("Enter pressed");	
+			return false;
+		}
+		
+		
+	});
+	
+	
+	//This method checks if the + button is pressed to add a to do to the list.
+	$('#addButton').on('click', function() {
+		
+		var foo = $("#toDoMessage").val();	
+		addToDo(foo);
+		console.log("Pressed the + button");
+		
+		return false;
+	});
+	
+	//This method removes a to do.
+	$('ul').on('click', 'li button.delete', function() {
+		
+		console.log("Remove button clicked.");
+		
+		//Er moet twee keer geklikt worden om het item echt te verwijderen.
+		if($(this).text() === "||") {
+			$(this).text("X")
+			$(this).focusin();
+		}  else if($(this).text() === "X") {
+			$(this).parent().remove();
+		}
+		
+		
+	});
+	
+	$('ul').on('blur', 'li button.delete', function() {
+		$(this).text("||");
+	});
+		
+});
