e5193d1276eeb715d3a4bc06569dd24f589b8d32
Lars Stegman <l.s.stegman@student.tudelft.nl>
2014-11-24 17:41:05 +0100

    Changed some shit

--- html/javascripts/scripts.js
+++ html/javascripts/scripts.js
@@ -1,43 +1,93 @@
 'use strict';
-//This method adds a to do and takes one vairable that is the message.
 
-var addToDo = function(message) {
+//This method adds a to do and takes one vairable that is the message.
+function addToDo() {
+
+	var message = getToDoMessage();
+	var date = getToDoDueDate();
+	
 	if(message !== "") {
 		var todo = "<li class='todo'><input type='checkbox' /><span contenteditable='true' >";
 		todo += message;
-		todo += "</span><br /><button class='delete'>| |</button></li>";
+		todo += "</span><button class='delete'>||</button><br />";
+		if(date !== null) {
+			todo += "<span class='duedate'>" + date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear();
+			
+			if(date.getHours() !== 0 || date.getMinutes() !== 0)  {
+				todo += " " + date.getHours() + ":" + date.getMinutes();
+			}
+			todo += "</span>";
+		}
+		
+		todo += "</li>";
+		
 		$("#todos ul").append(todo);
 		$("#toDoMessage").val("");
+		
+		if ( ($('.dateInput input[type=date]').val() !== "" ) || ( $('.dateInput input[type=time]').val() !== "") ) 
+			toggleDateEditor();
 	}
 	
 };
 
-$(document).ready(function () {
-	//This method checks if the enterkey is pressed to add a to do to the list.
+function toggleDateEditor() {
+	if($('.dateInput').is(':hidden')) {
+			$('.dateInput').show();
+		}
+		else {
+			$('.dateInput').hide();
+			$('.dateInput input[type=date]').val("");
+			$('.dateInput input[type=time]').val("");
+		}
+}
+
+function getToDoMessage() {
+	return $("#toDoMessage").val();
+}
+
+function getToDoDueDate() {
+	var date = $('.dateInput input[type=date]').val();
+	var time = $('.dateInput input[type=time]').val();
 	
-	$('#toDoMessage').on('keypress', function (key) {
+	if(date !== "") {
+		var year = date.slice(0,4);
+		var month = date.slice(5, 7) - 1;
+		var day = date.slice(8,10);
+
+		var hours = 0;
+		var minutes = 0;
+		if(time !== "") {
+			hours = time.slice(0,2);
+			minutes = time.slice(3, 5);
+		}
+
+		var newDate = new Date(year, month, day, hours, minutes);
+		console.log(newDate);
+		return newDate;
+	}
+	
+	return null;
+}
+
+$(document).ready(function () {
+	
+	$('.dateInput').hide();
+	
+	//This method checks if the enterkey is pressed to add a to do to the list.
+	$('#newToDo form').on('keypress', function (key) {
 		//Check if enter was pressed. If so, continue.
 		if (key.which === 13) {
-			
-			var foo = $("#toDoMessage").val();
-			
-			addToDo(foo);
-			
+			addToDo();
 			console.log("Enter pressed");
 			return false;
 		}
-		
-		
 	});
 	
 	
 	//This method checks if the + button is pressed to add a to do to the list.
 	$('#addButton').on('click', function() {
-		
-		var foo = $("#toDoMessage").val();
-		addToDo(foo);
+		addToDo();
 		console.log("Pressed the + button");
-		
 		return false;
 	});
 	
@@ -60,7 +110,8 @@ $(document).ready(function () {
 		});
 	});
 	
-	$('ul').on('click', 'li input[type="checkbox"]', function() {
+	//This method handles the checkbox behaviour.
+	$('ul').on('click', 'li input[type=checkbox]', function() {
 		console.log("checkbox clicked");
 		if($(this).is(':checked')) {
 			console.log("check");
@@ -73,6 +124,25 @@ $(document).ready(function () {
 		}
 	});
 		
+	//This methode listens if the enter key is pressed when a to do is edited.
+	$('ul').on('keypress', '.todo span', function(key) {
+		if(key.which === 13) {
+			console.log("BLUR IT!");
+			$(this).blur();
+			return false;
+		}
+	});
+	
+	//This method listens if the button to add a date is clicked.
+	$('#newToDo #addDate').on('click', function() {
+		toggleDateEditor();
+	});
+	
+	//This method listens if the date is clicked. if so 
+	$('ul').on('click', 'li .duedate', function() {
+			   
+			   
+	});
 		
 	
 });
